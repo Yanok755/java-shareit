3a54764cfd1ef48cbc6b93cef2ad71ebcbc978ca
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-06 23:42:27 +0300

    Update ItemRequestDto.java

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,29 @@
 package ru.practicum.shareit.request.dto;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ItemRequestDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @NotBlank(message = "Описание не может быть пустым")
+    private String description;
+
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long requestorId;
+
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime created;
 }

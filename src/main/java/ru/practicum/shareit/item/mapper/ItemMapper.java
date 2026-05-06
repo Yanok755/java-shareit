d9fb3269c60e2d92717b4f6976a5b871b9e0cfff
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-06 23:37:22 +0300

    Create ItemMapper.java

---
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -0,0 +1,42 @@
+package ru.practicum.shareit.item.mapper;
+
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemUpdateDto;
+import ru.practicum.shareit.item.model.Item;
+
+public class ItemMapper {
+    public static ItemDto toItemDto(Item item) {
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable(),
+                item.getRequest() != null ? item.getRequest().getId() : null);
+    }
+
+    public static Item toItem(ItemDto itemDto) {
+        Item item = new Item();
+        item.setId(itemDto.getId());
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setAvailable(itemDto.getAvailable());
+        return item;
+    }
+
+    public static ItemUpdateDto toItemUpdateDto(Item item) {
+        return new ItemUpdateDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable());
+    }
+
+    public static Item toItem(ItemUpdateDto itemUpdateDto) {
+        Item item = new Item();
+        item.setId(itemUpdateDto.getId());
+        item.setName(itemUpdateDto.getName());
+        item.setDescription(itemUpdateDto.getDescription());
+        item.setAvailable(itemUpdateDto.getAvailable());
+        return item;
+    }
+}

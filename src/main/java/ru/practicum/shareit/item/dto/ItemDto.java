3b0df20726a4c94f0b8ae07cb8ac09ea5eb55967
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-06 23:10:31 +0300

    Update ItemDto.java

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,31 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+/**
+ * TODO Sprint add-controllers.
+ */
 public class ItemDto {
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private Long id;
-
-    @NotBlank(message = "Имя не может быть пустым")
-    @Size(max = 100)
-    private String name;
-
-    @NotBlank(message = "Описание не может быть пустым")
-    @Size(max = 200)
-    private String description;
-
-    @NotNull(message = "Статус не может быть неопределен")
-    private Boolean available;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private Long requestId;
 }

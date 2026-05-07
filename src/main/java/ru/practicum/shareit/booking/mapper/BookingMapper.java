3ddfed3ab8298d7096d610332a23929b94f2a727
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-07 23:52:35 +0300

    Update BookingMapper.java

--- src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -1,12 +1,13 @@
 package ru.practicum.shareit.booking.mapper;
 
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.booking.dto.BookingCreateDto;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
 import ru.practicum.shareit.booking.model.Booking;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class BookingMapper {
     public static Booking toBooking(BookingCreateDto dto) {
         Booking booking = new Booking();
@@ -16,13 +17,21 @@ public class BookingMapper {
     }
 
     public static BookingResponseDto toBookingResponseDto(Booking booking) {
+        BookingResponseDto.ItemForBookingDto item = new BookingResponseDto.ItemForBookingDto(
+                booking.getItem().getId(),
+                booking.getItem().getName()
+        );
+        BookingResponseDto.BookerDto booker = new BookingResponseDto.BookerDto(
+                booking.getBooker().getId(),
+                booking.getBooker().getName()
+        );
         return new BookingResponseDto(
                 booking.getId(),
                 booking.getStart(),
                 booking.getEnd(),
                 booking.getStatus(),
-                booking.getItem().getId(),
-                booking.getBooker().getId()
+                item,
+                booker
         );
     }
 

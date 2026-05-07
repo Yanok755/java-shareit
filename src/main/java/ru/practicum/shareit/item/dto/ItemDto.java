d87f33cf3a69d7e8375616584ee028b73ecd04a8
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-08 00:02:06 +0300

    Update ItemDto.java

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import ru.practicum.shareit.booking.dto.BookingItemDto;
+import java.util.ArrayList;
+import java.util.List;
 
+
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 public class ItemDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
@@ -26,6 +29,8 @@ public class ItemDto {
     @NotNull(message = "Статус не может быть неопределен")
     private Boolean available;
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private Long requestId;
+    private BookingItemDto lastBooking;
+    private BookingItemDto nextBooking;
+
+    private List<CommentDto> comments = new ArrayList<>();
 }

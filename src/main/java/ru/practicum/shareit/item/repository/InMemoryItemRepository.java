5d6c6e7332de9f452b1bb887c526707c1611fbe3
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-07 11:57:35 +0300

    Update InMemoryItemRepository.java

--- src/main/java/ru/practicum/shareit/item/repository/InMemoryItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/InMemoryItemRepository.java
@@ -3,42 +3,34 @@ package ru.practicum.shareit.item.repository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 public class InMemoryItemRepository implements ItemRepository {
-    private final List<Item> items = new ArrayList<>();
+    private final Map<Long, Item> items = new HashMap<>();
     private long nextId = 1;
 
     @Override
     public Collection<Item> findAll() {
-        return List.copyOf(items);
+        return items.values();
     }
 
     @Override
     public Optional<Item> findItemById(Long itemId) {
-        return items.stream()
-                .filter(item -> item.getId() != null && item.getId().equals(itemId))
-                .findFirst();
+        return Optional.ofNullable(items.get(itemId));
     }
 
     @Override
     public Item save(Item item) {
         if (item.getId() == null) {
             item.setId(nextId++);
-        } else {
-            items.removeIf(existing -> existing.getId().equals(item.getId()));
         }
-
-        items.add(item);
+        items.put(item.getId(), item);
         return item;
     }
 
     @Override
     public void delete(Long itemId) {
-        items.removeIf(item -> item.getId() != null && item.getId().equals(itemId));
+        items.remove(itemId);
     }
 }

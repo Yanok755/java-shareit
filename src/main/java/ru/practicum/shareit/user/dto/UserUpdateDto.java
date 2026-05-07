0fdc9a87b9a5928386687273734ec36a68f420ed
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-08 00:11:29 +0300

    Update UserUpdateDto.java

--- src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
@@ -2,13 +2,12 @@ package ru.practicum.shareit.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 public class UserUpdateDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;

232ba2584f02eb7dbb615738067e9c0b985f5130
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-11 10:39:40 +0300

    Update ValidationUtils.java

--- src/main/java/ru/practicum/shareit/validation/ValidationUtils.java
+++ src/main/java/ru/practicum/shareit/validation/ValidationUtils.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
-public class ValidationUtils {
+public class EntityUtils {
     private final UserRepository userRepository;
     private final ItemRepository itemRepository;
 

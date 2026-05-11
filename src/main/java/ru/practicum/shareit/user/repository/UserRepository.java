6e67ff2b4c8e794688069558edb28678d603dbd5
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-11 10:24:59 +0300

    Update UserRepository.java

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -3,8 +3,9 @@ package ru.practicum.shareit.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.model.User;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    java.util.Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }

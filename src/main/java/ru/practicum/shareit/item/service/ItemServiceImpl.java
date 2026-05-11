d365e9926db0db85007f40481f9918159a3de350
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-11 10:42:50 +0300

    Update ItemServiceImpl.java

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -21,7 +21,7 @@ import ru.practicum.shareit.item.repository.CommentRepository;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
-import ru.practicum.shareit.validation.ValidationUtils;
+import ru.practicum.shareit.validation.EntityUtils;
 
 import java.time.LocalDateTime;
 import java.util.Collection;

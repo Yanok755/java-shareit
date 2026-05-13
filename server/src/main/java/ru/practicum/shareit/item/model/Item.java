ce01498558a76202bd3592fd3303910479dda954
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-13 12:30:54 +0300

    Create Item.java

---
+++ server/src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.item.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.practicum.shareit.user.model.User;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(exclude = "owner")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Boolean available;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @Column(name = "request_id")
+    private Long requestId;
+}

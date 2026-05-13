63a92b6aa7e0daa816d38b6d80b634911142ff62
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-13 12:29:57 +0300

    Create ItemMapper.java

---
+++ server/src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -0,0 +1,45 @@
+package ru.practicum.shareit.item.mapper;
+
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemUpdateDto;
+import ru.practicum.shareit.item.model.Item;
+
+public class ItemMapper {
+    public static ItemDto toItemDto(Item item) {
+        ItemDto dto = new ItemDto();
+        dto.setId(item.getId());
+        dto.setName(item.getName());
+        dto.setDescription(item.getDescription());
+        dto.setAvailable(item.getAvailable());
+        dto.setRequestId(item.getRequestId());
+
+        return dto;
+    }
+
+    public static Item toItem(ItemDto itemDto) {
+        Item item = new Item();
+        item.setId(itemDto.getId());
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setAvailable(itemDto.getAvailable());
+        item.setRequestId(itemDto.getRequestId());
+        return item;
+    }
+
+    public static ItemUpdateDto toItemUpdateDto(Item item) {
+        return new ItemUpdateDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable());
+    }
+
+    public static Item toItem(ItemUpdateDto itemUpdateDto) {
+        Item item = new Item();
+        item.setId(itemUpdateDto.getId());
+        item.setName(itemUpdateDto.getName());
+        item.setDescription(itemUpdateDto.getDescription());
+        item.setAvailable(itemUpdateDto.getAvailable());
+        return item;
+    }
+}

0b99a469432d9ce6d7e79c303e9122fcaa7ce9a7
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-13 12:29:37 +0300

    Create CommentMapper.java

---
+++ server/src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
@@ -0,0 +1,30 @@
+package ru.practicum.shareit.item.mapper;
+
+import ru.practicum.shareit.item.dto.CommentCreateDto;
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.model.Comment;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+public class CommentMapper {
+
+    public static Comment toComment(CommentCreateDto dto, Item item, User author) {
+        Comment comment = new Comment();
+        comment.setText(dto.getText());
+        comment.setItem(item);
+        comment.setAuthor(author);
+        comment.setCreated(LocalDateTime.now());
+        return comment;
+    }
+
+    public static CommentDto toCommentDto(Comment comment) {
+        return new CommentDto(
+                comment.getId(),
+                comment.getText(),
+                comment.getAuthor().getName(),
+                comment.getCreated()
+        );
+    }
+}

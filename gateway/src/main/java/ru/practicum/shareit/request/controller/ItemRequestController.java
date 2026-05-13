054ab2932be311ce8652a5345c8e2a419bb25001
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-13 13:21:48 +0300

    Update ItemRequestController.java

--- gateway/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -10,7 +10,7 @@ import ru.practicum.shareit.client.ItemRequestClient;
 import ru.practicum.shareit.request.dto.ItemRequestCreateDto;
 
 @Slf4j
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/requests")
 public class ItemRequestController {

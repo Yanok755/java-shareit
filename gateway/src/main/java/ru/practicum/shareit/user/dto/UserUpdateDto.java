fe181c463a84577f9909eb48c9e530cb22e4ee4e
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-13 12:03:22 +0300

    Create UserUpdateDto.java

---
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
@@ -0,0 +1,19 @@
+package ru.practicum.shareit.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    private String name;
+
+    @Email(message = "Некорректный формат email")
+    private String email;
+}

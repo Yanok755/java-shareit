b0c603701a2c8f381f3494eb112483677f5951fa
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-13 12:02:42 +0300

    Create UserDto..java

---
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto..java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @NotBlank(message = "Имя не может быть пустым")
+    private String name;
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Некорректный формат email")
+    private String email;
+}

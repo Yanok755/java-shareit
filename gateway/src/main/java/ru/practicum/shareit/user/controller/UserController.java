1085855c33950fd193c425ecfc2209794477e530
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-13 13:19:35 +0300

    Update UserController.java

--- gateway/src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -11,7 +11,7 @@ import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.dto.UserUpdateDto;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {

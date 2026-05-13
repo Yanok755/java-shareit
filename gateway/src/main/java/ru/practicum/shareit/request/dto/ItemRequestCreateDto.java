85a545f9212fbb9131fd01595a68ddc667368295
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-13 11:59:36 +0300

    Create ItemRequestCreateDto.java

---
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestCreateDto.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit.request.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemRequestCreateDto {
+
+    @NotBlank(message = "Описание запроса не может быть пустым")
+    private String description;
+}

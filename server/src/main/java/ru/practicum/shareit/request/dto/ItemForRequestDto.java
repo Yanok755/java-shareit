f389b3f4b1564170481c99b5b5518099435e54d5
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-14 13:55:36 +0300

    Update ItemForRequestDto.java

--- server/src/main/java/ru/practicum/shareit/request/dto/ItemForRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemForRequestDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.request.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,13 +10,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ItemForRequestDto {
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String name;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long ownerId;
 }

bc0d8a002cdb8631b90cc6c52474e3894a73bf11
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-05-14 12:41:28 +0300

    Update ItemMapper.java

--- server/src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -26,7 +26,6 @@ public class ItemMapper {
         item.setName(itemDto.getName());
         item.setDescription(itemDto.getDescription());
         item.setAvailable(itemDto.getAvailable());
-        item.setRequestId(itemDto.getRequestId());
         return item;
     }
 
